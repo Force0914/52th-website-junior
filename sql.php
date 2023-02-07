8f8a15c77309bd539818d3ec85c8eef7ccbda9f0
SmallDevil <a0923865232@gmail.com>
2023-02-07 10:11:32 +0800

    update

--- sql.php
+++ sql.php
@@ -1,8 +1,4 @@
 <?php
 $conn = new PDO("mysql:host=localhost;dbname=52_junior;charset=utf8;","admin","1234");
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$phone = $_POST['phone'];
-$password = $_POST['password'];
-$conn->query("INSERT INTO `tickets`(`first_name`, `last_name`, `phone`, `password`) VALUES ('$first_name','$last_name','$phone','$password')");
+$conn->query("INSERT INTO tickets(first_name, last_name, phone, password) VALUES ('{$_POST['first_name']}','{$_POST['last_name']}','{$_POST['phone']}','{$_POST['password']}')");
 header("Location: home.html");
